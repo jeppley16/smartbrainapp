f85ed5a83534e9ec5abdf15fd7bdc107bf92754a
jeppley16 <justin.eppley@gmail.com>
2018-07-27 13:39:47 -0400

    Sign In Route Finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex')
 
 const db = knex({
@@ -13,42 +14,15 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-	console.log(data);
-});
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// });
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'john',
-			password: 'cookies',
-			email: 'john@gmail.com',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'sally',
-			password: 'bananas',
-			email: 'sally@gmail.com',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-	{
-		id: '987',
-		hash: '',
-		email: 'john@gmail.com'
-	}
-	]
-}
 
 app.get('/', (req, res) => {
 	res.send(database.users);
@@ -56,27 +30,49 @@ app.get('/', (req, res) => {
 
 //Sign In Route for Application
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email && 
-		req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status(400).json('error logging in');
-	}
-	res.json('signin');
+	db.select('email', 'hash').from('login')  //no need for a transaction as we are just checking, not modifying database
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash); 
+			if (isValid) {
+				return db.select('*').from('users')  //always want to make sure we are returning
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])	
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+	.catch(err => res.status(400).json('wrong credentials'))
 })
 
 //Register Route for Application
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users')
-		.returning('*')  // from Knex, return all columns
-		.insert({ // insert user into database
-			email: email,
-			name: name,
-			joined: new Date()
-		})
-		.then(user => {
-			res.json(user[0]); //Because when registering, should only be one user		
+	const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {  //create a transaction when >1 thing to do at once, use trx object instead of db 
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx('users')
+					.returning('*')  // from Knex, return all columns
+					.insert({ // insert user into database
+						email: loginEmail[0],  //we want to return an array [0]
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						res.json(user[0]); //Because when registering, should only be one user		
+					})	
+			})
+			.then(trx.commit)  // to make sure the transaction is added to database
+			.catch(trx.rollback) // if anything fails, rollback changes
 		})
 		.catch(err => res.status(400).json('unable to register'))
 })
